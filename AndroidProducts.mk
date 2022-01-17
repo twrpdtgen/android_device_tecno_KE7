505297b4159c2facb32ae3fd19823e2b14068abf
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-17 21:20:59 +0100

    KE7: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 1 Manufacturer: tecno Model: TECNO KE7 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_KE7.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_KE7-user \
+    omni_KE7-userdebug \
+    omni_KE7-eng
